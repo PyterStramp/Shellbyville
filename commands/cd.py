79b5aa1f92ab4b2d7a59092a090a7b098cc10cf8
Santiago Riano <santiagoriano68@gmail.com>
2024-07-07 15:23:38 -0500

    modificado y aprobado comando cd

--- commands/cd.py
+++ commands/cd.py
@@ -1,9 +1,23 @@
 import os
 
-def change_directory(path):
-    try:
-        os.chdir(path)
-        return os.getcwd()  # Devuelve el nuevo directorio de trabajo actual
-    except OSError as e:
-        print(f"Error cambiando directorio: {e}")
+def change_directory(path, flag, base_path, current_path):
+    if not path:
         return None
+    
+    if path.startswith('/'):
+        new_path = os.path.abspath(os.path.join(base_path, path.lstrip('/'))) # ruta absoluta para base_path
+    else:
+        new_path = os.path.abspath(os.path.join(current_path, path))
+    if flag == '-P':
+        new_path = os.path.realpath(new_path)  # enlace simbólico al path real
+
+    if new_path.startswith(base_path):
+        try:
+            os.chdir(new_path)
+            return os.getcwd()  # devuelve el nuevo directorio
+        except OSError as e:
+            print(f"Error cambiando directorio: {e}")
+    else:
+        print("El directorio está fuera de la base de Shellbyville")
+    
+    return None
--- main.py
+++ main.py
@@ -11,35 +11,53 @@ def clear_screen_before_execute():
     # Limpiar la pantalla antes de
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def execute_command(command, current_path):
+def execute_command(command, current_path, base_path):
     parts = command.split()
     if parts:
         if parts[0] == 'cd':
+            flag = '-L'  # Config por default
+            path = None
+            
             if len(parts) > 1:
-                new_path = change_directory(parts[1])
+                if parts[1] in ['-L', '-P']:
+                    flag = parts[1]
+                    if len(parts) > 2:
+                        path = parts[2]
+                    else:
+                        print("El comando CD requiere el modificador -L o -P ")
+                        return current_path
+                else:
+                    path = parts[1]
+                
+                new_path = change_directory(path, flag, base_path, current_path)
                 if new_path:
-                    return new_path # devolver directorio.jsp
+                    return new_path  # devolver el nuevo directorio
             else:
-                print("cd command requires a path argument.")
+                print("Se requiere una dirección antes de cambiar directorio")
+                
         elif parts[0] == 'ls':
             list_directory()
         elif parts[0] == 'echo':
             print_message(' '.join(parts[1:]))
         else:
-            print("Command not implemented.")
-    return current_path  # Devolver directorio actual.js
+            print("Comando no implementado/reconocido.")
+    return current_path  # devolver el directorio actual
 
 def main():
     clear_screen_before_execute()  # Limpiar la pantalla al inicio
-    print(Fore.CYAN + "Te damos la bienvenida a Shellbyville" + Style.RESET_ALL)
-    current_path = os.getcwd() # dir actual
+
+    base_path = os.path.abspath(os.getcwd())
+    current_path = base_path  # dir base de Shellbyville
+    print(Fore.CYAN + "Esperando órdenes" + Style.RESET_ALL)
+
     while True:
-        current_dir = os.path.basename(current_path) if current_path != '/' else '/'
-        prompt = f"<{Fore.GREEN}Shellbyville:{Fore.BLUE}{current_dir}{Style.RESET_ALL}> "
+        relative_path = os.path.relpath(current_path, base_path)
+        prompt = f"{Fore.GREEN}<Shellbyville:{Fore.BLUE}{relative_path if relative_path != '.' else ''}{Style.RESET_ALL}> "
         command = input(prompt)
         if command.lower() in ['exit', 'quit']:
+            print(Fore.CYAN + "Nos hemos ido" + Style.RESET_ALL)
             break
-        current_path = execute_command(command, current_path)
+        current_path = execute_command(command, current_path, base_path)
 
 if __name__ == "__main__":
     main()
